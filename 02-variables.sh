45fb4ebdb5312aa8563dbc6d7daf71b0d60c5721
SarikaMunurathi <sarikaammu458@gmail.com>
2024-07-16 20:34:13 +0530

    by sarika

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Ramesh ::Hai How are You Suresh??"
+echo "Suresh ::Iam Good How about You Ramesh??"
+echo "Ramesh ::Iam also good,How is your work Now"
+echo "Suresh ::Not bad..Thinking to upgrade with DevOps"
