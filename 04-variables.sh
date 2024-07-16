642e2f2a8982deb6cbf9bfa0e64472f42323cb76
SarikaMunurathi <sarikaammu458@gmail.com>
2024-07-16 21:05:31 +0530

    by sarika

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$PERSON1 ::Hai How are You $PERSON2??"
+echo "$PERSON2 ::Iam Good How about You $PERSON1??"
+echo "$PERSON1 ::Iam also good,How is your work Now"
+echo "$PERSON2 ::Not bad..Thinking to upgrade with DevOps"
