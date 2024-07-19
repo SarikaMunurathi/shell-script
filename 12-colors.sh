04a2fe824a5e9280b9dafdcb25ef8c306a199170
SarikaMunurathi <sarikaammu458@gmail.com>
2024-07-19 07:34:05 +0530

    by sarika

--- 12-colors.sh
+++ 12-colors.sh
@@ -3,10 +3,12 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$CRIPTNAME/$TIMESTAMP.log
+LOGFILE=/tmp/$CRIPTNAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+
+echo "Script started executing at: $TIMESTAMP"
 
 VALIDATE(){
   if [ $1 -ne 0]
